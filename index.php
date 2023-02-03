f30cf1c6c3232ecf6277ce78c9c0b7c884df5f31
Carlos Varela <caduvarela@gmail.com>
2023-02-03 11:48:15 -0300

    Prototipando Design e Fontes
    Definindo um design inicial e importando fontes.

--- index.php
+++ index.php
@@ -4,11 +4,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="./css/style.css">
+    <title>Home</title>
 </head>
-<body>
+<body class="dark">
     <header>
-        <h1>Olá, Mundo!</h1>
+
     </header>
+    <article>
+        <div>
+            <h1>Titulo</h1>
+        </div>
+        <div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+    </article>
 </body>
 </html>
